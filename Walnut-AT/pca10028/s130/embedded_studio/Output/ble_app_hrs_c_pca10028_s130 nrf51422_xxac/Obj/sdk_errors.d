2c84f36548cb394266dbbf44e12130941c0d6c36
Thomas <thomas@iMac.local>
2017-02-06 01:49:10 +0100

    deleted more from Output directory

--- Walnut-AT/pca10028/s130/embedded_studio/Output/ble_app_hrs_c_pca10028_s130 nrf51422_xxac/Obj/sdk_errors.d
+++
@@ -1,7 +0,0 @@
-Output/ble_app_hrs_c_pca10028_s130\ nrf51422_xxac/Obj/sdk_errors.o: \
- /Users/thomas/GitHub/nRF5x_AT/Walnut-AT/pca10028/s130/embedded_studio/../../../../nRF5_SDK_12/components/libraries/util/sdk_errors.c \
- /Users/thomas/GitHub/nRF5x_AT/Walnut-AT/pca10028/s130/embedded_studio/../../../../nRF5_SDK_12/components/libraries/util/sdk_errors.h \
- /Applications/SEGGER\ Embedded\ Studio\ 3.10i/include/stdint.h \
- ../../../../nRF5_SDK_12/components/softdevice/s130/headers/nrf_error.h \
- ../config/sdk_config.h \
- /Users/thomas/GitHub/nRF5x_AT/Walnut-AT/pca10028/s130/embedded_studio/../../../../nRF5_SDK_12/components/libraries/util/nordic_common.h
--- Walnut-AT/pca10028/s130/embedded_studio/Output/ble_app_hrs_c_pca10028_s130 nrf51422_xxac/Obj/thumb_crt0.d
+++
@@ -1,2 +0,0 @@
-Output/ble_app_hrs_c_pca10028_s130\ nrf51422_xxac/Obj/thumb_crt0.o: \
- /Users/thomas/GitHub/nRF5x_AT/Walnut-AT/pca10028/s130/embedded_studio/thumb_crt0.s
